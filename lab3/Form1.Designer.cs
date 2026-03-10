9c38ade0151dc8e39f642769375bcfab57ca926e
Addy <addycarram@gmail.com>
2026-03-10 15:39:54 +0200

    creating the coverting system

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainbtn
+            // 
+            this.mainbtn.Location = new System.Drawing.Point(622, 219);
+            this.mainbtn.Name = "mainbtn";
+            this.mainbtn.Size = new System.Drawing.Size(294, 75);
+            this.mainbtn.TabIndex = 0;
+            this.mainbtn.Text = "Converteste";
+            this.mainbtn.UseVisualStyleBackColor = true;
+            this.mainbtn.Click += new System.EventHandler(this.mainbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Introduceti numerarul";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Valuta ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "MDL",
+            "USD",
+            "EUR",
+            "RON",
+            "RUBLE"});
+            this.comboBox1.Location = new System.Drawing.Point(428, 271);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(134, 33);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "MDL",
+            "USD",
+            "EUR",
+            "RON",
+            "RUBLE"});
+            this.comboBox2.Location = new System.Drawing.Point(432, 339);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(134, 33);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Valuta converta";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(428, 219);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 31);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 407);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Suma convertita";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(434, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "se converteste";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1135, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 50);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Chitanta\r\n\r\n";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1135, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Din Valuta\r\n";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1135, 339);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 25);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Exchange in\r\n";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1135, 407);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 25);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Comision";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1135, 470);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(143, 50);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Eliberat suma\r\n\r\n";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(1394, 271);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(111, 25);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "se incarca";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(1394, 339);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(111, 25);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "se incarca\r\n";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(1394, 407);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(111, 25);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "se incarca";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(1394, 470);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(111, 25);
+            this.label15.TabIndex = 19;
+            this.label15.Text = "se incarca";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(2505, 982);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mainbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button mainbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ lab3/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void mainbtn_Click(object sender, EventArgs e)
+        {
+            //double i=int.Parse(textBox1.Text);
+            //if (fromcomboBox1.SelectedItem.ToString() == "MDL" &&
+            //    tocomboBox2.SelectedItem.ToString() == "USD" )
+            //{
+            //    double conver = i / 16.56;
+            //    label5.Text = "Suma obtinuta: " + conver + "\t$";
+            //}
+            double i = int.Parse(textBox1.Text);
+            if (i > 0)
+            {
+                textBox1.BackColor = Color.DarkOliveGreen;
+                label5.BackColor=Color.White;
+                if (comboBox1.SelectedItem.ToString() == "MDL" &&
+                    comboBox2.SelectedItem.ToString() == "USD")
+                {
+                    double conver = i / 16.56;
+                    label5.Text = "Suma obținuta: " + conver + "\t $";
+                    label12.Text = "MDL";
+                    label13.Text = "USD";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+
+                }
+                if (comboBox1.SelectedItem == "MDL" && comboBox2.SelectedItem == "EUR")
+                {
+                    double conver = i / 19.84;
+                    label5.Text = "Suma obținuta: " + conver + "\t euro";
+                    label12.Text = "MDL";
+                    label13.Text = "EUR";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "EUR" &&
+                    comboBox2.SelectedItem.ToString() == "MDL")
+                {
+                    double conver = i * 19.84;
+                    label5.Text = "Suma obținuta: " + conver + "\t Lei";
+                    label12.Text = "EUR";
+                    label13.Text = "MDL";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "USD" &&
+                    comboBox2.SelectedItem.ToString() == "MDL")
+                {
+                    double conver = i * 16.56;
+                    label5.Text = "Suma obținuta: " + conver + "\t Lei";
+                    label12.Text = "USD";
+                    label13.Text = "MDL";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "EUR" &&
+                    comboBox2.SelectedItem.ToString() == "USD")
+                {
+                    double conver = i * 1.17;
+                    label5.Text = "Suma obținuta: " + conver + "\t USD";
+                    label12.Text = "EUR";
+                    label13.Text = "USD";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "RON" &&
+                    comboBox2.SelectedItem.ToString() == "EUR")
+                {
+                    double conver = i * 0.2;
+                    label5.Text = "Suma obținuta: " + conver + "\t EUR";
+                    label12.Text = "RON";
+                    label13.Text = "EUR";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+
+                if (comboBox1.SelectedItem.ToString() == "EUR" &&
+                    comboBox2.SelectedItem.ToString() == "RON")
+                {
+                    double conver = i * 5.10;
+                    label5.Text = "Suma obținuta: " + conver + "\t RON";
+                    label12.Text = "EUR";
+                    label13.Text = "RON";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "MDL" &&
+                    comboBox2.SelectedItem.ToString() == "RON")
+                {
+                    double conver = i * 0.25;
+                    label5.Text = "Suma obținuta: " + conver + "\t RON";
+                    label12.Text = "MDL";
+                    label13.Text = "RON";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "RON" &&
+                    comboBox2.SelectedItem.ToString() == "MDL")
+                {
+                    double conver = i * 3.8;
+                    label5.Text = "Suma obținuta: " + conver + "\t MDL";
+                    label12.Text = "RON";
+                    label13.Text = "MDL";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "RON" &&
+                    comboBox2.SelectedItem.ToString() == "USD")
+                {
+                    double conver = i * 0.25;
+                    label5.Text = "Suma obținuta: " + conver + "\t USD";
+                    label12.Text = "RON";
+                    label13.Text = "USD";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+
+                if (comboBox1.SelectedItem.ToString() == "USD" &&
+                    comboBox2.SelectedItem.ToString() == "RON")
+                {
+                    double conver = i * 4.3;
+                    label5.Text = "Suma obținuta: " + conver + "\t RON";
+                    label12.Text = "USD";
+                    label13.Text = "RON";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "MDL" &&
+                    comboBox2.SelectedItem.ToString() == "RUBLE")
+                {
+                    double conver = i * 4.5;
+                    label5.Text = "Suma obținuta: " + conver + "\t RUBLE";
+                    label12.Text = "MDL";
+                    label13.Text = "RUBLE";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "RUBLE" &&
+                    comboBox2.SelectedItem.ToString() == "MDL")
+                {
+                    double conver = i * 0.22;
+                    label5.Text = "Suma obținuta: " + conver + "\t MDL";
+                    label12.Text = "RUBLE";
+                    label13.Text = "MDL";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "RUBLE" &&
+                    comboBox2.SelectedItem.ToString() == "EUR")
+                {
+                    double conver = i * 0.011;
+                    label5.Text = "Suma obținuta: " + conver + "\t EUR";
+                    label12.Text = "RUBLE";
+                    label13.Text = "EUR";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+
+                if (comboBox1.SelectedItem.ToString() == "RUBLE" &&
+                   comboBox2.SelectedItem.ToString() == "USD")
+                {
+                    double conver = i * 0.013;
+                    label5.Text = "Suma obținuta: " + conver + "\t USD";
+                    label12.Text = "RUBLE";
+                    label13.Text = "USD";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+
+                if (comboBox1.SelectedItem.ToString() == "RUBLE" &&
+                   comboBox2.SelectedItem.ToString() == "RON")
+                {
+                    double conver = i * 0.056;
+                    label5.Text = "Suma obținuta: " + conver + "\t RON";
+                    label12.Text = "RUBLE";
+                    label13.Text = "RON";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "RON" &&
+                   comboBox2.SelectedItem.ToString() == "RUBLE")
+                {
+                    double conver = i * 17.76;
+                    label5.Text = "Suma obținuta: " + conver + "\t RUBLE";
+                    label12.Text = "RON";
+                    label13.Text = "RUBLE";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+
+                if (comboBox1.SelectedItem.ToString() == "EUR" &&
+                   comboBox2.SelectedItem.ToString() == "RUBLE")
+                {
+                    double conver = i * 90.36;
+                    label5.Text = "Suma obținuta: " + conver + "\t RUBLE";
+                    label12.Text = "EUR";
+                    label13.Text = "RUBLE";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+                if (comboBox1.SelectedItem.ToString() == "USD" &&
+                   comboBox2.SelectedItem.ToString() == "RUBLE")
+                {
+                    double conver = i * 76.25;
+                    label5.Text = "Suma obținuta: " + conver + "\t RUBLE";
+                    label12.Text = "USD";
+                    label13.Text = "RUBLE";
+                    label14.Text = "0";
+                    label15.Text = "" + conver;
+                }
+            }
+            else
+            {
+                label5.Text = " ati introdus nr negative";
+                label5.BackColor = Color.Red;
+                textBox1.BackColor = Color.Red;
+            }
+
+
+
+
+
+        }
+    }
+}
