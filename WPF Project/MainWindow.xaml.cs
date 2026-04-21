d04bd3789ad05fe71bac6d2ab7d09f27fc432be0
Addy <addycarram@gmail.com>
2026-04-21 23:36:56 +0300

    interface and the logic of the calculator

--- WPF Project/MainWindow.xaml.cs
+++ WPF Project/MainWindow.xaml.cs
@@ -20,5 +20,49 @@ namespace WPF_Project
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(text1.Text) || string.IsNullOrWhiteSpace(text2.Text))
+                {
+                MessageBox.Show("Fill the text box");
+                return;
+
+            }
+            if(!int.TryParse(text1.Text, out int nr1))
+            {
+                MessageBox.Show(" Enter a valid number");
+                return;
+            }
+            if (!int.TryParse(text2.Text, out int nr2))
+            {
+                MessageBox.Show(" Enter a valid number");
+                return;
+            }
+            switch (combo.SelectedItem.ToString()) {
+                case "Plus":
+                    {
+                        rezult.Content = (nr1 + nr2).ToString();
+                        break;
+                    }
+                case "Minus":
+                    {
+                        rezult.Content = (nr1 - nr2).ToString();
+                        break;
+                    }
+                case "Multiplication":
+                    {
+                        rezult.Content = (nr1 * nr2).ToString();
+                        break;
+                    }
+                case "Divider":
+                    {
+                        rezult.Content = (nr1 / nr2).ToString();
+                        break;
+                    }
+            }
+            
+        }
     }
 }
